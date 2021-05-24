58bb9270f905bb3a8198457915d2c82993c911d7
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2021-05-24 15:44:25 +0200

    Add files via upload

---
+++ AdrianDashboard_usingcsv.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 10:06:29 2021
+@author: lpeikert
+"""
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import pickle
+import altair as alt
+
+staticcapital = pd.read_csv('/Users/lpeikert/Desktop/rest-clients-demo-master/python/staticcapital.csv')  
+staticcapital['Date'] = pd.to_datetime(staticcapital['Date'], format='%Y-%m-%d')
+staticcapital = staticcapital.set_index('Date')
+
+workingcapital = pd.read_csv('/Users/lpeikert/Desktop/rest-clients-demo-master/python/workingcapital.csv')  
+workingcapital['Date'] = pd.to_datetime(workingcapital['Date'], format='%Y-%m-%d')
+workingcapital = workingcapital.set_index('Date')
+
+totalvalue = pd.read_csv('/Users/lpeikert/Desktop/rest-clients-demo-master/python/totalvalue.csv')  
+totalvalue['Date'] = pd.to_datetime(totalvalue['Date'], format='%Y-%m-%d')
+totalvalue = totalvalue.set_index('Date')
+
+kpis = pd.read_csv('/Users/lpeikert/Desktop/rest-clients-demo-master/python/kpis.csv')
+kpis['Date'] = pd.to_datetime(kpis['Date'], format='%Y-%m-%d')
+kpis = kpis.set_index('Date')
+
+
+
+st.set_page_config(page_title = 'Streamlit Dashboard', layout='wide', page_icon='💹')
+st.header('Adrians BTC Dashboard')
+
+st.button('I am just a gimmick button but lucas did not program me yet')
+
+### top row 
+
+st.markdown("## Main KPIs")
+st.markdown(f"as of {totalvalue.index[-1]}")
+
+first_kpi, second_kpi, third_kpi, fourth_kpi, fifth_kpi, sixth_kpi = st.beta_columns(6)
+
+
+with first_kpi:
+    st.markdown("**Total Gain in AED**")
+    number1 = round(totalvalue.iloc[-1,2],3)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number1}</h1>", unsafe_allow_html=True)
+
+with second_kpi:
+    st.markdown("**Total Gain in %**")
+    number2 = round(totalvalue.iloc[-1,4],3)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number2}</h1>", unsafe_allow_html=True)
+
+with third_kpi:
+    st.markdown("**Daily Gain in AED**")
+    number3 = round(totalvalue.iloc[-1,3],3)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number3}</h1>", unsafe_allow_html=True)
+    
+with fourth_kpi:
+    st.markdown("**Daily Gain in %**")
+    number4 = round(totalvalue.iloc[-1,5],3)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number4}</h1>", unsafe_allow_html=True)
+
+with fifth_kpi:
+    st.markdown("**Average productivity BTC p.d.**")
+    number5 = round(kpis.iloc[-1,2],6)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number5}</h1>", unsafe_allow_html=True)
+
+with sixth_kpi:
+    st.markdown("**Daily productivity BTC p.d.**")
+    number6 = round(kpis.iloc[-1,1],6)
+    st.markdown(f"<h1 style='text-align: left; color: red;'>{number6}</h1>", unsafe_allow_html=True)
+
+st.markdown("<hr/>", unsafe_allow_html=True)
+
+
+
+st.markdown("## Total Gain and Total Value vs Invested")
+
+first_chart, second_chart = st.beta_columns(2)
+
+with first_chart:
+    chart_data = totalvalue['%Total Gain']
+    st.bar_chart(chart_data)
+
+     
+with second_chart:
+    df = totalvalue
+    df.reset_index(drop=True, inplace=True)
+    chart_data = df[['Total Value', 'Total invested']]
+    st.area_chart(chart_data)
+
+    
+    
+
+st.markdown("<hr/>", unsafe_allow_html=True)
+
+
+st.markdown("forecast yearly return")
+
+st.line_chart(totalvalue[['% cummulating p.a.', '% average p.a.']])
+
+st.markdown("<hr/>", unsafe_allow_html=True)
+
+st.markdown("Total Value")
+st.dataframe(totalvalue)
+#st.table(totalvalue)
+
+st.markdown("workingc apital")
+st.dataframe(workingcapital)
+
+st.markdown("static capital")
+st.dataframe(staticcapital)
+
+
+
+"""
+#Analysis
+"""
+# Sensitivity Analysis
+
+#sensitivityrates = np.array([0.05, 0.15, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 1.85, 1.95])
+dailyprofitability = kpis.iloc[-1,1]
+
+
+sensitivityrates = np.arange(0.2, 2, 0.2)
+sensitivityrates2 = np.arange(0.6, 1.5, 0.1)
+rateBTCAED = sensitivityrates * (workingcapital.iloc[-1, 0])
+rateprofitability = sensitivityrates * dailyprofitability
+
+parameters = []
+paras = []
+parameters = pd.DataFrame(parameters, columns = rateBTCAED, index = rateprofitability)
+for i in rateBTCAED:
+    for o in rateprofitability:
+        parameters[o, i] = (((((i*o-workingcapital.iloc[-1,4])*30)/totalvalue.iloc[-1,0])+1)**(365/30)-1)
+        paras.append(((((i*o*30)/totalvalue.iloc[-1,0])+1)**(365/30)-1))
+
+rateBTCAEDlist = 100 * np.repeat(sensitivityrates,len(sensitivityrates))
+rateprofitabilitylist = 100 * np.tile(sensitivityrates2,len(sensitivityrates2))
+heatmap = pd.DataFrame({'values': paras, 'exchange rate BTCAED %': rateBTCAEDlist, 'profitability rate %': rateprofitabilitylist})
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pvt = pd.pivot_table(pd.DataFrame(heatmap),
+    values='values', index='exchange rate BTCAED %', columns='profitability rate %')
+ax = sns.heatmap(pvt, annot=True, annot_kws={"size": 7}, vmax = 3, cmap="YlGnBu")
+
+st.write(ax)
+st.pyplot()
---
+++ Adriantablesanddashboardsusingcsv.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 13:50:21 2021
+@author: lpeikert
+"""
+
+# Import the libraries
+import pandas as pd
+import numpy as np
+import datetime
+import yfinance as yf
+import math
+import nicehash
+import pickle
+
+# Initialize dates
+
+startdate = datetime.datetime(2021, 5, 15)
+today = datetime.datetime.now()
+today = datetime.datetime(today.year, today.month, today.day)
+delta = datetime.timedelta(days=1)
+delta2 = datetime.timedelta(days=2)
+
+# Read data
+
+staticcapital = pd.read_csv('https://github.com/Mentalustig/Bitcoindashboard/blob/main/staticcapital.csv')  
+staticcapital['Date'] = pd.to_datetime(staticcapital['Date'], format='%Y-%m-%d')
+staticcapital = staticcapital.set_index('Date')
+
+workingcapital = pd.read_csv('https://github.com/Mentalustig/Bitcoindashboard/blob/main/workingcapital.csv')  
+workingcapital['Date'] = pd.to_datetime(workingcapital['Date'], format='%Y-%m-%d')
+workingcapital = workingcapital.set_index('Date')
+
+totalvalue = pd.read_csv('https://github.com/Mentalustig/Bitcoindashboard/blob/main/totalvalue.csv')  
+totalvalue['Date'] = pd.to_datetime(totalvalue['Date'], format='%Y-%m-%d')
+totalvalue = totalvalue.set_index('Date')
+
+kpis = pd.read_csv('https://github.com/Mentalustig/Bitcoindashboard/blob/main/kpis.csv')
+kpis['Date'] = pd.to_datetime(kpis['Date'], format='%Y-%m-%d')
+kpis = kpis.set_index('Date')
+
+staticcapital.loc[today] = [0,0,0,0] if staticcapital.index[-1] != today else print('all good')
+workingcapital.loc[today] = [0,0,0,0,0,0,0] if workingcapital.index[-1] != today else print('all good')
+totalvalue.loc[today] = [0,0,0,0,0,0,0,0,0] if totalvalue.index[-1] != today else print('all good')
+kpis.loc[today] = [0,0,0] if kpis.index[-1] != today else print('all good')
+
+
+
+# Download Wallet Infos
+
+#OrganizationID = 06ca7b90-1477-40da-b502-596683869ad4
+#API Key Code = 02ef110f-6b4a-4269-b206-6f75a291eddd
+#API Secret Key Code = b4b1cda2-4c7a-4dc2-9ce7-33be0a12995082494b92-c3f9-4e02-a752-c49d458db8db
+
+### Production  
+#API key code: c4c5a286-c2e2-4a61-9094-347001abff3e
+#API secret code: f59745af-ffdb-4c52-9ece-793e119d436869cb7c1b-d286-4d92-ae14-052ad06cb39f
+#organizationID = 2652eea1-7c24-4350-99d2-7ed86608635a
+
+#TEST
+#host = 'https://api-test.nicehash.com'
+#organisation_id = '06ca7b90-1477-40da-b502-596683869ad4'
+#key = '02ef110f-6b4a-4269-b206-6f75a291eddd'
+#secret = 'b4b1cda2-4c7a-4dc2-9ce7-33be0a12995082494b92-c3f9-4e02-a752-c49d458db8db' 
+
+#PROD
+host = 'https://api2.nicehash.com'
+organisation_id = '2652eea1-7c24-4350-99d2-7ed86608635a'
+key = 'c4c5a286-c2e2-4a61-9094-347001abff3e'
+secret = 'f59745af-ffdb-4c52-9ece-793e119d436869cb7c1b-d286-4d92-ae14-052ad06cb39f' 
+
+
+public_api = nicehash.public_api(host)
+#buy_info = public_api.buy_info()
+#print(buy_info)
+
+private_api = nicehash.private_api(host, organisation_id, key, secret)
+
+my_accounts = private_api.get_accounts_for_currency('BTC')
+
+totalbalance = (my_accounts['totalBalance'])
+
+print('totalBTCbalance =', totalbalance)
+
+activeworkers = private_api.get_active_workers(100, 0, 'RIG_NAME', 'ASC')
+activeworkers = activeworkers['workers']
+
+amount_active_rigs = len(activeworkers)
+print('amount_active_rigs =', amount_active_rigs)
+
+rigs = pd.DataFrame(list(activeworkers))
+rigs['Date'] = today
+rigs = rigs.set_index('Date')
+
+getrigIDs = private_api.get_groups_list(True)
+
+rigdata = pd.DataFrame(list(getrigIDs['groups']['']['rigs']))
+
+getrig = []
+getrig = pd.DataFrame(getrig, index = rigdata['rigId'])
+for i in rigdata['rigId']:
+    getrig.at[i, 'name'] = private_api.get_rig2(i)['name']
+    getrig.at[i, 'profitability'] = private_api.get_rig2(i)['profitability']
+
+for i in rigdata['rigId']:
+    for o in range(0, 6):
+        basename = "powerusage"
+        columnname = basename + "_" + str(o+1)
+        try:
+            getrig.at[i, columnname] = private_api.get_rig2(i)['devices'][o]['powerUsage']
+        except IndexError:
+          #  getrig.at[i, columnname] = 'null'
+            print('Oops, I did not find power usage for', i)
+
+dailyprofitability = getrig['profitability'].sum()
+
+powerlist= list(getrig)
+powerlist.remove('name')
+powerlist.remove('profitability')
+
+getrig['totalpowerusage'] = getrig[powerlist].sum(axis=1)
+totalpowerusage = (getrig['totalpowerusage'].sum(axis=0)/1000)*24
+
+
+
+"""
+NOT NEEDED ANYMORE
+# Download market data
+df_yahoo_BTCUSD = yf.download('BTC-USD', start='2021-05-15', progress='False')
+df_yahoo_BTCUSD = df_yahoo_BTCUSD['Adj Close']
+df_yahoo_USDAED = yf.download('AED=X', start='2021-05-15', progress='False')
+df_yahoo_USDAED = df_yahoo_USDAED['Adj Close']
+# initialize list of lists
+wc = pd.concat([df_yahoo_BTCUSD, df_yahoo_USDAED], axis=1)
+wclist = [[0, 0, 0, 0]]
+workingcapital = pd.DataFrame(wclist)
+workingcapital = pd.concat([wc, workingcapital], axis=1)
+workingcapital.columns = ['Exchange Rate BTC/USD', 'Exchange Rate USD/AED', 'Amount BTC', 'Electricity Costs', 'Total Electricity Costs', 'Money taken out']
+workingcapital.index.names = ['Date']
+workingcapital = workingcapital.iloc[1:]
+while startdate <= today :
+    if math.isnan(workingcapital['Exchange Rate USD/AED'][startdate]) :
+        workingcapital['Exchange Rate USD/AED'][startdate] = workingcapital['Exchange Rate USD/AED'][startdate-delta]
+        if math.isnan(workingcapital['Exchange Rate USD/AED'][startdate-delta]) :
+            workingcapital['Exchange Rate USD/AED'][startdate] = workingcapital['Exchange Rate USD/AED'][startdate-delta2]
+    startdate += delta
+startdate = datetime.datetime(2021, 5, 15)
+df_ER_BTCUSD = workingcapital['Exchange Rate BTC/USD']*workingcapital['Exchange Rate USD/AED']
+df_ER_BTCUSD = pd.DataFrame(df_ER_BTCUSD, columns = ['Exchange Rate BTC/AED'])
+workingcapital = pd.concat([df_ER_BTCUSD, workingcapital], axis=1)
+staticcapital = workingcapital.copy()
+staticcapital[['Amount Invested', 'Amount Reinvested', 'Depreciation Rigs', 'Total Value Rigs']] = ['','','','']
+staticcapital = staticcapital[['Amount Invested', 'Amount Reinvested', 'Depreciation Rigs', 'Total Value Rigs']]
+totalvalue = workingcapital.copy()
+totalvalue[['Total invested', 'Total Value', 'Total Gain', 'Total Daily Gain', '%Total Gain', '%Daily Gain', '% cummulating p.a.', '% average p.a.', '']] = ['', '', '', '', '', '', '', '', '']
+totalvalue = totalvalue[['Total invested', 'Total Value', 'Total Gain', 'Total Daily Gain', '%Total Gain', '%Daily Gain', '% cummulating p.a.', '% average p.a.', '']]
+kpis = workingcapital.copy()
+kpis[['Productivity per rig', 'Daily productivity', 'Average productivity']] = ['', '', '']
+kpis = kpis[['Productivity per rig', 'Daily productivity', 'Average productivity']]
+"""
+
+#clean up all float type integers
+
+staticcapital["Amount Invested"] = pd.to_numeric(staticcapital["Amount Invested"], downcast="float")
+staticcapital["Total Value Rigs"] = pd.to_numeric(staticcapital["Total Value Rigs"], downcast="float")
+workingcapital['Amount BTC'] = pd.to_numeric(workingcapital['Amount BTC'], downcast="float")
+totalvalue["% average p.a."] = pd.to_numeric(totalvalue["% average p.a."], downcast="float")
+totalvalue["% cummulating p.a."] = pd.to_numeric(totalvalue["% cummulating p.a."], downcast="float")
+
+
+"""
+NOT NEEDED ANYMORE
+# when did we buy rigs
+staticcapital['Amount Invested'][datetime.datetime(2021, 5, 15)] = 10000
+staticcapital['Amount Invested'][datetime.datetime(2021, 5, 20)] = 30000
+staticcapital['Amount Invested'][datetime.datetime(2021, 5, 22)] = 70000
+for i in range (0, int(((today-startdate).days)+2)):
+    try:
+        workingcapital.iloc[i, 5] = workingcapital.iloc[:i+1, 4].sum()
+    except IndexError:
+        print('Oops, I did not find any electricity data')
+# calculate how much depreciation we incur
+depreciation = float(-1/(365*5))
+totalvalue['Total Gain'] = totalvalue['Total Value'] - totalvalue['Total invested']
+totalvalue['%Total Gain'] = (totalvalue['Total Gain']/totalvalue['Total invested'])*100
+for i in range (0, int(((today-startdate).days)+2)):
+    if totalvalue.iloc[i, 0] != 0:
+        totalvalue.iloc[i, 6] = ((totalvalue.iloc[i, 2]/totalvalue.iloc[i, 0] +1 ) ** (365/i) -1) * 100
+for i in range (0, int(((today-startdate).days)+2)):
+    if totalvalue.iloc[i, 0] != 0:
+        totalvalue.iloc[i, 7] = ((totalvalue.iloc[i, 2]/totalvalue.iloc[i, 0]) * (365/i)) * 100
+for i in range (0, int(((today-startdate).days)+2)):
+    try:
+        staticcapital.iloc[i, 2] = staticcapital.iloc[:i+1, 0].sum() * depreciation
+    except IndexError:
+        print('Oops, I did not find any more days', i)
+for i in range (0, int(((today-startdate).days)+2)):
+    try:
+        staticcapital.iloc[i, 3] = staticcapital.iloc[:i+1, 0].sum()+staticcapital.iloc[:i+1, 2].sum()
+    except IndexError:
+        print('Oops, I made a mistake for value rigs calculation')
+# Getting overall value
+for i in range (0, int(((today-startdate).days)+2)):
+    try:
+        totalvalue.iloc[i, 0] = staticcapital.iloc[:i+1, 0].sum()
+    except IndexError:
+        print('Oops, I made a mistake for total invested calculation')
+for i in range (0, int(((today-startdate).days)+2)):
+    try:
+        # totalvalue = rig value + btc value + electricity costs accumulated + 
+        totalvalue.iloc[i, 1] = staticcapital.iloc[i, 3]+(0 if math.isnan(workingcapital.iloc[i, 3]) else workingcapital.iloc[i, 3]*workingcapital.iloc[i,0])+workingcapital.iloc[i, 5]
+    except IndexError:
+        print('Oops, I made a mistake for total invested calculation')
+staticcapital.to_pickle("staticcapital.pkl")  # saved old dataframes
+workingcapital.to_pickle("workingcapital.pkl")  # saved old dataframes
+totalvalue.to_pickle("totalvalue.pkl")  # saved old dataframes
+kpis.to_pickle("kpis.pkl")  # saved old dataframes
+"""
+
+
+"""
+INSERT CODE TO GET NEW VALUES FROM THE YAHOO API
+"""
+
+# Download market data
+
+df_yahoo_BTCUSD = yf.download('BTC-USD', start=startdate, progress='False')
+df_yahoo_BTCUSD = df_yahoo_BTCUSD['Adj Close']
+
+df_yahoo_USDAED = yf.download('AED=X', start=startdate, progress='False')
+df_yahoo_USDAED = df_yahoo_USDAED['Adj Close']
+
+
+workingcapital['Exchange Rate BTC/USD'][today] = df_yahoo_BTCUSD.iloc[-1]
+
+if df_yahoo_USDAED.iloc[-1] > 0:
+    workingcapital['Exchange Rate USD/AED'][today] = df_yahoo_USDAED.iloc[-1]
+else:
+    workingcapital['Exchange Rate USD/AED'][today] = workingcapital.iloc[-2,2]
+
+workingcapital['Exchange Rate BTC/AED'][today] = workingcapital.iloc[-1, 1]*workingcapital.iloc[-1, 2]
+
+
+"""
+DID WE BUY NEW RIGS?
+"""
+# when did we buy rigs
+#staticcapital['Amount Invested'][datetime.datetime(YEAR, MONTH, DAY)] = SUMME INVESTED
+
+
+"""
+INSERT CODE TO GET NEW VALUES FROM RIGS & make tables up to date
+"""
+
+workingcapital['Amount BTC'][today] = float(totalbalance)
+kpis['Daily productivity'][today] = float(dailyprofitability)
+kpis['Average productivity'][today] = float(workingcapital['Amount BTC'][today]/int((today-startdate).days))
+
+cost_elec_per_rig_per_watt = 0.26
+workingcapital['Electricity Costs'][today] = cost_elec_per_rig_per_watt*totalpowerusage
+
+
+try:
+    workingcapital.iloc[-1, 5] = workingcapital.iloc[:, 4].sum()
+except IndexError:
+    print('Oops, I did not find any electricity data')
+
+depreciation = -1/(365*5)
+
+try:
+    staticcapital.iloc[-1, 2] = staticcapital.iloc[:, 0].sum() * depreciation
+except IndexError:
+    print('Oops, I did not find any more days')
+    
+try:
+    staticcapital.iloc[-1, 3] = staticcapital.iloc[:, 0].sum()+staticcapital.iloc[:, 2].sum()
+except IndexError:
+    print('Oops, I made a mistake for value rigs calculation')
+
+# Getting overall value
+try:
+    totalvalue.iloc[-1, 0] = staticcapital.iloc[:, 0].sum()
+except IndexError:
+    print('Oops, I made a mistake for total invested calculation')
+
+
+try:
+    # totalvalue = rig value + btc value + electricity costs accumulated + 
+    totalvalue.iloc[-1, 1] = staticcapital.iloc[-1, 3]+(0 if math.isnan(workingcapital.iloc[-1, 3]) else workingcapital.iloc[-1, 3]*workingcapital.iloc[-1,0])+workingcapital.iloc[-1, 5]
+except IndexError:
+    print('Oops, I made a mistake for total invested calculation')
+    
+totalvalue['Total Gain'] = totalvalue['Total Value'] - totalvalue['Total invested']
+totalvalue['Total Daily Gain'] = totalvalue['Total Gain'] - totalvalue['Total Gain'].shift(1)
+totalvalue['%Daily Gain'] = (totalvalue['Total Daily Gain']/totalvalue['Total invested']) * 100
+totalvalue['%Total Gain'] = (totalvalue['Total Gain']/totalvalue['Total invested']) * 100
+totalvalue.iloc[-1, 6] = ((totalvalue.iloc[-1, 2]/totalvalue.iloc[-1, 0] +1 ) ** (365/(today-startdate).days) -1) * 100
+totalvalue.iloc[-1, 7] = ((totalvalue.iloc[-1, 2]/totalvalue.iloc[-1, 0]) * (365/(today-startdate).days)) * 100
+
+# Save data into new data sets
+
+staticcapital.to_csv("https://github.com/Mentalustig/Bitcoindashboard/blob/main/staticcapital.csv", index=True)
+workingcapital.to_csv("https://github.com/Mentalustig/Bitcoindashboard/blob/main/workingcapital.csv", index=True)
+totalvalue.to_csv("https://github.com/Mentalustig/Bitcoindashboard/blob/main/totalvalue.csv", index=True)
+kpis.to_csv("https://github.com/Mentalustig/Bitcoindashboard/blob/main/kpis.csv", index=True)
